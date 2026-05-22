e166a435422fd4e525ecce32d118e26fb39287b6
lee <nhattruongta13@gmail.com>
2026-05-22 12:33:54 -0500

    start

---
+++ backend/rag-api/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+const PORT = process.env.PORT
+
+
+
+app.listen(PORT, () => console.log(`The server is living on port ${PORT}`))
